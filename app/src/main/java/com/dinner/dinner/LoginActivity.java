341fd7e4c926fa1c11e667c6474152c1f17fc2b8
mantas <>
2020-01-10 13:32:22 +0200

    remember me

--- app/src/main/java/com/dinner/dinner/LoginActivity.java
+++ app/src/main/java/com/dinner/dinner/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.dinner.dinner;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -19,6 +21,14 @@ public class LoginActivity extends AppCompatActivity {
         final EditText password = findViewById(R.id.password);
         Button loginBtn = findViewById(R.id.login_btn);
         Button registerBtn = findViewById(R.id.register_btn);
+
+        final SharedPreferences preferences = getApplicationContext().getSharedPreferences("RememberMe", 0);
+        final SharedPreferences.Editor editor = preferences.edit();
+        final CheckBox rememberMe = findViewById(R.id.remember_me);
+
+        if(preferences.contains("Login")) {
+            username.setText(preferences.getString("Login", null));
+        }
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +46,14 @@ public class LoginActivity extends AppCompatActivity {
                     username.setError(getResources().getString(R.string.login_invalid_credentials_message));
                     username.requestFocus();
                 }
+               if (rememberMe.isChecked()) {
+                   editor.putString("Login", username2);
+                   editor.apply();
+               } else {
+                   editor.putString("Login", "");
+                   editor.apply();
+               }
+
                 //----------------------------------------------------iš kur-------------į kur---------//
                 if (validUserName && validPassword) {
                     Toast.makeText(LoginActivity.this, "Welcome back, : " + username2, Toast.LENGTH_SHORT).show();
